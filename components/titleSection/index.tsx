73d5b6a8170c7a36fd538fdc57289081c65b181d
Bernardo <bernardowerty@outlook.com>
2026-02-06 16:04:28 -0300

    adicionando estilo props no componente titleSection para modularismo

--- components/titleSection/index.tsx
+++ components/titleSection/index.tsx
@@ -4,11 +4,12 @@ interface TitleSectionProps{
     subtitle:string;
     title:string;
     description:string;
+    className?:string;
 }
 
-export function TitleSection({subtitle, title, description}:TitleSectionProps){
+export function TitleSection({subtitle, title, description, className}:TitleSectionProps){
     return(
-         <div className="text-left tablet:text-center space-y-4">
+         <div className={`text-left tablet:text-center space-y-4 ${className ? className : ""}`}>
             <Subtitle>{subtitle}</Subtitle>
             <h2 className="font-lato font-semibold text-2xl tablet:text-5xl text-black-500">{title}</h2>
             <p className="text-sm tablet:text-base text-grey-500">{description}</p>
