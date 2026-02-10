ba3ac13413db36259e290b2e5262cc9b27429cc6
Bernardo <bernardowerty@outlook.com>
2026-02-10 16:14:29 -0300

    ajustes no responsivo do footer

--- app/components/footer/index.tsx
+++ app/components/footer/index.tsx
@@ -7,7 +7,7 @@ export function Footer(){
         <footer className="pt-20 pb-6">
             <ContainerGRID>
                  <InfoTop/>
-                 <p className="text-center mt-20 pt-6 border-t border-grey-400">Copyright © 2035 Hirezy. All Rights Reserved.</p>
+                 <p className="text-center text-sm tablet:text-base mt-12 tablet:mt-20 pt-6 border-t border-grey-400">Copyright © 2035 Hirezy. All Rights Reserved.</p>
             </ContainerGRID>
         </footer>
     )
