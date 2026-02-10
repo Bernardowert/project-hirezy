deb0b51cf8a6fedddc3a49290e665041dabccce6
Bernardo <bernardowerty@outlook.com>
2026-02-10 01:51:58 -0300

    adicionando funcionalidade de scroll no header fixado

--- app/components/header/index.tsx
+++ app/components/header/index.tsx
@@ -1,3 +1,5 @@
+'use client';
+
 import Link from "next/link";
 import { ContainerGRID } from "../containerGRID";
 import Image from "next/image";
@@ -7,10 +9,23 @@ import logo from "@/app/assets/logo.png";
 import { NavListing } from "./navListing";
 import { Button } from "../button";
 import { BtnMobile } from "./btnMobile";
+import { useEffect, useState } from "react";
 
 export function Header(){
+    const[scroll,setScroll] = useState(0);
+
+    useEffect(() =>{
+          
+        const handleScroll = () =>{
+            setScroll(window.scrollY);
+        }
+
+        window.addEventListener('scroll', handleScroll);
+
+        return () => window.removeEventListener('scroll', handleScroll);
+    }, []);
     return(
-        <header className="py-6 w-full fixed top-0 left-0 z-50">
+        <header className={`py-6 w-full fixed top-0 left-0 z-50 bg-white transition-all ease-linear ${scroll > 0 ? 'shadow-lg' : ''}`}>
             <ContainerGRID className="flex items-center justify-between">
                 <Link href="/">
                    <Image
