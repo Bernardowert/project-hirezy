6d63e97270764497dcbeebdaed51519cc6496426
Bernardo <bernardowerty@outlook.com>
2026-02-12 17:51:15 -0300

    finalização da section faq com responsivo

--- app/components/accordionFAQ/index.tsx
+++ app/components/accordionFAQ/index.tsx
@@ -33,10 +33,10 @@ export function AccordionFAQ({classname}: {classname?:string}){
          setOpenIndex(prev => prev === index ? null : index);
     }
     return(
-        <div className={`space-y-6 w-full ${classname ? classname : ""}`}>
+        <div className={`space-y-4 laptop:space-y-6 w-full ${classname ? classname : ""}`}>
                {
                  listingFAQ.map(({title, description}, index) => (
-                    <div className={`p-6 laptop:px-8 rounded-2xl transition-colors ease-linear ${openIndex === index ? "bg-blue-300" : "bg-white"}`} key={`${title + `${index}`}`}>
+                    <div className={`py-6 px-1 laptop:px-8 rounded-2xl transition-colors ease-linear ${openIndex === index ? "bg-blue-300" : "bg-white"}`} key={`${title + `${index}`}`}>
                         <button className="flex items-center justify-between w-full" onClick={() => toggleAccordion(index)}>
                              <h3 className="text-sm tablet:text-base laptop:text-lg text-black-500 font-medium">{title}</h3>
                              <ChevronUp size={20} color="#222222" className={`transition-transform ease-linear ${openIndex === index ? "rotate-0" : "rotate-180"}`}/>
--- app/components/sections/home/faq/index.tsx
+++ app/components/sections/home/faq/index.tsx
@@ -6,27 +6,27 @@ import { ArrowRight } from "lucide-react";
 
 export function SectionFaq(){
     return(
-        <section>
-            <ContainerGRID className="flex items-center justify-between">
-                  <div className="w-full max-w-112.5">
+        <section className="pt-10 pb-62.75 laptop:py-20">
+            <ContainerGRID className="flex items-start flex-col gap-8 laptop:gap-0 laptop:flex-row laptop:items-center justify-between relative laptop:static">
+                  <div className="w-full laptop:max-w-112.5">
                       <TitleSection 
                       subtitle="FAQ" 
                       title="Have a Question?" 
                       description="Save time with straightforward answers to common questions recruiters and HR teams often ask."
                       isTxtCenter={false}
                       />
-                      <div className="w-full max-w-93 h-50.25 p-6 bg-bgForm bg-cover bg-no-repeat rounded-2xl mt-50.75">
-                           <strong className="text-2xl font-lato font-semibold text-black-500 block">Got more questions?</strong>
-                           <p className="opacity-95 mt-2 mb-8">Get in touch and we ll take care of the rest.</p>
-                           <Button className="flex items-center gap-4 py-2.5">
+                      <div className="w-full h-44.75 laptop:h-50.25 max-w-[90%] tablet:max-w-93  p-6 bg-bgForm bg-cover bg-no-repeat rounded-2xl laptop:mt-50.75 absolute -bottom-52.25 laptop:static">
+                           <strong className="text-xl laptop:text-2xl font-lato font-semibold text-black-500 block">Got more questions?</strong>
+                           <p className="opacity-95 text-sm laptop:text-base mt-2 mb-8">Get in touch and we ll take care of the rest.</p>
+                           <Button className="flex items-center gap-4 py-2 laptop:py-2.5">
                              <span>Contact Us</span>
-                             <div className="size-10 flex items-center justify-center bg-white rounded-full transition-colors ease-linear group-hover:bg-black-500 group-focus:bg-black-500">
-                                  <ArrowRight size={18} color="#222222" className="transition-all ease-linear group-hover:stroke-white group-focus:stroke-white"/>
+                             <div className="size-5.5 laptop:size-10 flex items-center justify-center bg-white rounded-full transition-colors ease-linear group-hover:bg-black-500 group-focus:bg-black-500">
+                                  <ArrowRight size={18} color="#222222" className="transition-all ease-linear group-hover:stroke-white group-focus:stroke-white max-w-3 laptop:max-w-full"/>
                              </div>
                            </Button>
                       </div>
                   </div>
-                  <AccordionFAQ classname="max-w-182.5"/>
+                  <AccordionFAQ classname="laptop:max-w-182.5"/>
             </ContainerGRID>
         </section>
     )
