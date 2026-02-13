58a2658f441e327fb307b6ccb3a58fbac8940dd7
Bernardo <bernardowerty@outlook.com>
2026-02-13 12:58:56 -0300

    limpando imports desnecessarios na pagina home

--- app/page.tsx
+++ app/page.tsx
@@ -1,18 +1,12 @@
-import { CardAudience } from "@/app/components/cardAudience";
 import { SectionAudience } from "@/app/components/sections/home/audience";
 import { SectionHero } from "@/app/components/sections/home/hero";
 import { SectionAbout } from "@/app/components/sections/home/s-About";
-import { Subtitle } from "@/app/components/subtitle";
-import { CardValue } from "./components/cardValue";
 import { SectionValue } from "./components/sections/home/value";
-import { BannerHiring } from "./components/bannerHiring";
-import { ContainerGRID } from "./components/containerGRID";
 import { SectionHiringHome } from "./components/sections/home/hiring";
 import { SectionNumbers } from "./components/sections/home/numbers";
 import { SectionFeatures } from "./components/sections/home/features";
 import { SectionIntegration } from "./components/sections/home/integration";
 import { SectionPrice } from "./components/sections/home/price";
-import { AccordionFAQ } from "./components/accordionFAQ";
 import { SectionFaq } from "./components/sections/home/faq";
 import { SectionTestimonals } from "./components/sections/home/testimonals";
 
