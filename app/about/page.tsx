c3168664098ab0eeefba21ab6d47888206ea390a
Bernardo <bernardowerty@outlook.com>
2026-02-13 13:13:45 -0300

    criação inicial da pagina about, criação inicial da section hero about

---
+++ app/about/page.tsx
@@ -0,0 +1,9 @@
+import { SectionHeroAbout } from "../components/sections/about/heroAbout";
+
+export default function About(){
+    return(
+        <>
+          <SectionHeroAbout/>
+        </>
+    )
+}
---
+++ app/components/sections/about/heroAbout.tsx
@@ -0,0 +1,12 @@
+import { ContainerGRID } from "../../containerGRID";
+
+export function SectionHeroAbout(){
+    return(
+          <section className="bg-bgHeroAbout bg-cover bg-no-repeat bg-center h-101 flex items-center">
+            <ContainerGRID className="text-center">
+                <h1 className="text-48/tight font-lato font-semibold text-black-500 mb-5 max-w-161 mx-auto">Empowering Companies to Hire Smarter and Grow Faster</h1>
+                <p className="max-w-161 mx-auto opacity-90">Our platform bridges technology and talent, helping organizations scale efficiently with smarter hiring decisions.</p>
+            </ContainerGRID>
+          </section>
+    )
+}
