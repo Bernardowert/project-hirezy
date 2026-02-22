5e68f3f22c38539b644f6a3d355f47095f2616ed
Bernardo <bernardowerty@outlook.com>
2026-02-22 09:13:11 -0300

    ajustando padding interno na section Map na pagina about, ajustes de css

--- app/components/sections/contact/map/index.tsx
+++ app/components/sections/contact/map/index.tsx
@@ -3,7 +3,7 @@ import { Maps } from "./maps";
 
 export function SectionMap(){
     return(
-        <section className="py-20">
+        <section className="py-10 laptop:py-20">
             <ContainerGRID>
                    <div className="text-center space-y-4 mb-8 tablet:mb-14">
                        <span className="text-sm tablet:text-base inline-block py-2 px-4 rounded-2xl bg-blue-300 text-grey-500">Map</span>
